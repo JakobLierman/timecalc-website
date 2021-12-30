e5ea5d95cccddbd363ff7adfd22dae64ab695cb6
Jakob Lierman <jakob.lierman@gmail.com>
2021-12-30 14:44:25 +0100

    refactor: move footer icons to constants

--- src/components/footer/Footer.component.tsx
+++ src/components/footer/Footer.component.tsx
@@ -1,44 +1,17 @@
 import React, { FC } from 'react';
 import { useTranslation } from 'next-i18next';
 import { getYear } from 'date-fns';
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import { UrlObject } from 'url';
-import { IconType } from 'react-icons';
 import { EI18nNamespace } from '@public/locales/i18n.type';
 import { TComponentProps } from '@/components/component.types';
 import FooterIcon from '@/components/footerIcon/FooterIcon.component';
+import iconValues from '@/constants/footerIcons.constant';
 import Styled from './Footer.styled';
 
 type TProps = TComponentProps;
 
-type TFooterIcon = {
-	name: string;
-	href: string | UrlObject;
-	component: IconType;
-};
-
 const Footer: FC<TProps> = ({ className }) => {
 	const { t } = useTranslation(EI18nNamespace.Common);
 	const year = getYear(new Date());
-
-	const iconValues: TFooterIcon[] = [
-		// TODO: Enable website in footer
-		// {
-		// 	name: 'Website',
-		// 	href: 'https://jakoblierman.be',
-		// 	component: AiFillHome,
-		// },
-		{
-			name: 'LinkedIn',
-			href: 'https://www.linkedin.com/in/jakoblierman/',
-			component: AiFillLinkedin,
-		},
-		{
-			name: 'GitHub',
-			href: 'https://github.com/JakobLierman',
-			component: AiFillGithub,
-		},
-	];
 
 	return (
 		<Styled.Container className={className}>
---
+++ src/constants/footerIcons.constant.ts
@@ -0,0 +1,30 @@
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { UrlObject } from 'url';
+import { IconType } from 'react-icons';
+
+type TFooterIcon = {
+	name: string;
+	href: string | UrlObject;
+	component: IconType;
+};
+
+const iconValues: TFooterIcon[] = [
+	// TODO: Enable website in footer
+	// {
+	// 	name: 'Website',
+	// 	href: 'https://jakoblierman.be',
+	// 	component: AiFillHome,
+	// },
+	{
+		name: 'LinkedIn',
+		href: 'https://www.linkedin.com/in/jakoblierman/',
+		component: AiFillLinkedin,
+	},
+	{
+		name: 'GitHub',
+		href: 'https://github.com/JakobLierman',
+		component: AiFillGithub,
+	},
+];
+
+export default iconValues;
